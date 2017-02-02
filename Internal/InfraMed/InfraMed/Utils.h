d6c9213269ada4385b23d35b8ca31c6820e8a91f
avi <avi@medial-research.com>
2017-02-02 16:49:10 +0200

    initial ideas for Universal Signals

--- Internal/InfraMed/InfraMed/Utils.h
+++ Internal/InfraMed/InfraMed/Utils.h
@@ -25,5 +25,11 @@ int read_bin_file_IM(string &fname, unsigned char* &data, unsigned long long &si
 int read_bin_file_IM_parallel(string &fname, unsigned char* &data, unsigned long long &size);
 int write_bin_file_IM(string &fname, unsigned char* data, unsigned long long size);
 
+// forced to keep a copy of these inside in order NOT to depend on external libraries
+// for now assuming an int is enough
+// All will be computed from 1/1/1900
+//int date_to_days_IM(int date);
+//int date_to_hours_IM(int date) { return 24*date_to_days_IM(date); };
+//int date_to_minutes_IM(int date) { return 24*60*date_to_days_IM(date); }
 
 #endif
