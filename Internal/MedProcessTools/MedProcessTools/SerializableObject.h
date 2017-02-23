5d55731e554d95f40c0df0dec71c5e57bede99df
avi <avi@medial-research.com>
2017-02-23 17:13:25 +0200

    MedProcess: initial easier tools to serialize under namespace MedSerialize

--- Internal/MedProcessTools/MedProcessTools/SerializableObject.h
+++ Internal/MedProcessTools/MedProcessTools/SerializableObject.h
@@ -5,6 +5,7 @@
 
 #include "Logger/Logger/Logger.h"
 #include "MedUtils/MedUtils/MedIO.h"
+#include <cstring>
 
 using namespace std;
 
@@ -26,5 +27,28 @@ public:
 	void init_from_string(string init_string);
 	virtual int  init(map<string, string>& map) { return 0; }
 };
+
+
+// helpers to common serialization
+//namespace MedSerialize {
+//
+//	// templated simple ones : int, float, long, double, etc...
+//	template <class T> size_t get_size(T &elem);
+//	template <class T> size_t serialize(unsigned char *blob, T &elem);
+//	template <class T> size_t deserialize(unsigned char *blob, T &elem);
+//
+//	// string is special
+//	template<> size_t get_size(string &str);
+//	template<> size_t serialize(unsigned char *blob, string &str);
+//	template<> size_t deserialize(unsigned char *blob, string &str);
+//
+//	// vector of type T that has a MedSerialize function
+//	template <class T> size_t get_size(vector<T> &v);
+//	template <class T> size_t serialize(unsigned char *blob, vector<T> &v);
+//	template <class T> size_t deserialize(unsigned char *blob, vector<T> &v);
+//
+//}
+
+#include "SerializableObject_imp.h"
 
 #endif
