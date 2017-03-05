d24859dd8392caafad45952cbca7ed090b63cf66
yaron kinar <yaron@medial-research.com>
2017-03-05 18:47:13 +0200

    correct merge

--- Internal/InfraMed/InfraMed/MedPidRepository.cpp
+++ Internal/InfraMed/InfraMed/MedPidRepository.cpp
@@ -586,8 +586,6 @@ int PidDynamicRec::set_version_off_orig(int sid, int version)
 		if (curr_len + size > data_size)
 			resize_data(2*(data_size + size));
 		
-		void *datap = (void *)&data[pl->pos];
-
 		PosLen new_pl;
 		new_pl.pos = curr_len;
 		new_pl.len = len;
