8eac56174b5ae57a45f77cedbe6e49a0f3927097
ido hadanny <ido@medial-research.com>
2017-10-26 19:04:57 +0300

    added 10 attempts to read a model due to unrepeatable error in our unix nodes mounts :(

--- Internal/MedProcessTools/MedProcessTools/SerializableObject.cpp
+++ Internal/MedProcessTools/MedProcessTools/SerializableObject.cpp
@@ -4,36 +4,51 @@
 #include "MedProcessUtils.h"
 #include <assert.h>
 #include <boost/crc.hpp>
+#include <chrono>
+#include <thread>
+
+#ifndef _MSC_VER
+#include <unistd.h>
+#endif
 
 #define LOCAL_SECTION LOG_SRL
 #define LOCAL_LEVEL	LOG_DEF_LEVEL
 
 // read and deserialize model
 int SerializableObject::read_from_file(const string &fname) {
+	int attempts = 0;
 	unsigned char *blob;
 	unsigned long long size;
+	for (;;) {
+		try {
+			if (read_binary_data_alloc(fname, blob, size) < 0) {
+				MERR("Error reading model from file %s\n", fname.c_str());
+				return -1;
+			}
 
-	if (read_binary_data_alloc(fname, blob, size) < 0) {
-		MERR("Error reading model from file %s\n", fname.c_str());
-		return -1;
+			boost::crc_32_type checksum_agent;
+			checksum_agent.process_bytes(blob, size);
+			MLOG("read_from_file [%s] with crc32 [%d]\n", fname.c_str(), checksum_agent.checksum());
+
+			int vers = *((int*)blob);
+			int s = sizeof(int);
+			if (vers != version())
+				MTHROW_AND_ERR("deserialization error. code version %d. requested file version %d\n",
+					version(), vers);
+			unsigned char *blob_without_version = blob + s;
+
+			size_t serSize = deserialize(blob_without_version);
+			assert(serSize == size);
+			if (size > 0) delete[] blob;
+			return 0;
+		}
+		catch (exception e) {
+			if (attempts++ >= 10)
+				throw e;
+			MWARN("[%d] attempt to read [%s] failed with [%s], retyring...\n", attempts, fname.c_str(), e.what());
+			std::this_thread::sleep_for(chrono::seconds(10));
+		}
 	}
-
-	std::size_t const blob_len = sizeof(blob) / sizeof(blob[0]);
-	boost::crc_32_type checksum_agent;
-	checksum_agent.process_bytes(blob, blob_len);
-	MLOG("read_from_file [%s] with crc32 [%d]\n", fname.c_str(), checksum_agent.checksum());
-
-	int vers = *((int*)blob);
-	int s = sizeof(int);
-	if (vers != version())
-		MTHROW_AND_ERR("deserialization error. code version %d. requested file version %d\n",
-			version(), vers);
-	unsigned char *blob_without_version = blob + s;
-
-	size_t serSize = deserialize(blob_without_version);
-	assert(serSize == size);
-	if (size > 0) delete[] blob;
-	return 0;
 }
 
 // serialize model and write to file
