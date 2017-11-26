07b8eb73f2850c41073ffaf4c08819bf223476c2
avi <avi@medial-research.com>
2017-11-26 17:31:31 +0200

    made sure thrown exceptions are passing openmp ... unfortunately this does not happen by default

--- Internal/MedProcessTools/MedProcessTools/MedModel.cpp
+++ Internal/MedProcessTools/MedProcessTools/MedModel.cpp
@@ -159,7 +159,7 @@ int MedModel::apply(MedPidRepository& rep, MedSamples& samples, MedModelStage st
 
 	// Stage Sanity
 	if (end_stage <=  MED_MDL_APPLY_FTR_GENERATORS) {
-		MERR("MedModel apply() : Illegal end stage %d\n",end_stage);
+		MERR("MedModel apply() : Illegal end stage %d\n", end_stage);
 		return -1;
 	}
 
@@ -249,7 +249,7 @@ int MedModel::generate_features(MedPidRepository &rep, MedSamples *samples, vect
 		generator->init(features);
 	// preparing records and features for threading
 	int N_tot_threads = omp_get_max_threads();
-//	MLOG("MedModel::learn/apply() : feature generation with %d threads\n", N_tot_threads);
+	//	MLOG("MedModel::learn/apply() : feature generation with %d threads\n", N_tot_threads);
 	vector<PidDynamicRec> idRec(N_tot_threads);
 	features.init_all_samples(samples->idSamples);
 
@@ -265,26 +265,36 @@ int MedModel::generate_features(MedPidRepository &rep, MedSamples *samples, vect
 
 	// Loop on ids
 	int RC = 0;
+	int thrown = 0;
 #pragma omp parallel for schedule(dynamic)
 	for (int j = 0; j<samples->idSamples.size(); j++) {
-		MedIdSamples& pid_samples = samples->idSamples[j];
-		int n_th = omp_get_thread_num();
-		int rc = 0;
+		try {
+			MedIdSamples& pid_samples = samples->idSamples[j];
+			int n_th = omp_get_thread_num();
+			int rc = 0;
 
-		// Generate DynamicRec with all relevant signals
-		if (idRec[n_th].init_from_rep(std::addressof(rep), pid_samples.id, req_signals, (int)pid_samples.samples.size()) < 0) rc = -1;
-		// Apply rep-cleaning
+			// Generate DynamicRec with all relevant signals
+			if (idRec[n_th].init_from_rep(std::addressof(rep), pid_samples.id, req_signals, (int)pid_samples.samples.size()) < 0) rc = -1;
+			// Apply rep-cleaning
 
-		for (auto& processor : rep_processors)
-			if (processor->apply(idRec[n_th], pid_samples) < 0) rc = -1;
+			for (auto& processor : rep_processors)
+				if (processor->apply(idRec[n_th], pid_samples) < 0) rc = -1;
 
-		// Generate Features
-		for (auto& generator : _generators)
-			if (generator->generate(idRec[n_th], features) < 0) rc = -1;
-//#pragma omp critical 
-		if (rc < 0) RC = -1;
+			// Generate Features
+			for (auto& generator : _generators)
+				if (generator->generate(idRec[n_th], features) < 0) rc = -1;
+			//#pragma omp critical 
+			if (rc < 0) RC = -1;
+		}
+		catch (int &i_e) {
+			// have to catch each thread
+			if (i_e < thrown) thrown = i_e;
+		}
 	}
+
+	if (thrown < 0) throw thrown; // throwing if needed
 	return RC;
+
 }
 
 //.......................................................................................
