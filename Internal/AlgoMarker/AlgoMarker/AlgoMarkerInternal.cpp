edc168fe628cd41fbe56723003e03fbfac7d2c01
avi <avi@medial-research.com>
2017-12-12 15:03:30 +0200

    more work done on input testers

--- Internal/AlgoMarker/AlgoMarker/AlgoMarkerInternal.cpp
+++ Internal/AlgoMarker/AlgoMarker/AlgoMarkerInternal.cpp
@@ -12,12 +12,17 @@ InputTester *InputTester::make_input_tester(InputTesterType it_type)
 //-------------------------------------------------------------------------------------------------------------------------
 void InputTesterSimple::input_from_string(const string &in_str)
 {
-	
+	sf.init_from_string(in_str);
 }
 
 //-------------------------------------------------------------------------------------------------------------------------
 // 1: good to go 0: did not pass -1: could not test
 int InputTesterSimple::test_if_ok(MedRepository &rep, int pid, long long timestamp, int &nvals, int &noutliers)
 {
+	MedSample s;
 
+	s.id = pid;
+	s.time = (int)timestamp;
+
+	return sf.test_filter(s, rep, nvals, noutliers);
 }
