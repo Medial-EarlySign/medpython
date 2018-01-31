84d4fd4c76c1e222b89d264cb8d495339a779dee
Alon Lanyado <alon@medial-research.com>
2018-01-31 16:40:46 +0200

    add param to probability caliberation and doc

--- Internal/MedAlgo/MedAlgo/MedAlgo.h
+++ Internal/MedAlgo/MedAlgo/MedAlgo.h
@@ -193,13 +193,44 @@ public:
 			"isn't supported for " + model_name + " yet.");
 	};
 
-	///caliberation for probability using training:
+	/// <summary>
+	/// caliberation for probability using training data
+	/// @param x The training matrix
+	/// @param y The Labels
+	/// @param min_bucket_size The minimal observations to create probabilty bin
+	/// @param min_score_jump The minimal diff in scores to create bin
+	/// @param min_prob_jump The minimal diff in probabilties to create bin
+	/// @param fix_prob_order If true will unite bins that are sorted in wrong way
+	/// </summary>
+	/// <returns>
+	/// @param min_range - writes a corresponding vector with minimal score range
+	/// @param max_range - writes a corresponding vector with maximal score range
+	/// @param map_prob - writes a corresponding vector with probabilty for score range
+	/// </returns>
 	int learn_prob_calibration(MedMat<float> &x, vector<float> &y,
 		vector<float> &min_range, vector<float> &max_range, vector<float> &map_prob, int min_bucket_size = 10000,
-		float min_score_jump = 0.001, float min_prob_jump = 0.005);
+		float min_score_jump = 0.001, float min_prob_jump = 0.005, bool fix_prob_order = false);
+	/// <summary>
+	/// If you have ran learn_prob_calibration before, you have min_range,max_range,map_prob from
+	/// This function - that is used to convert preds to probs
+	/// </summary>
 	int convert_scores_to_prob(const vector<float> &preds, const vector<float> &min_range,
 		const vector<float> &max_range, const vector<float> &map_prob, vector<float> &probs);
+	/// <summary>
+	/// Will create probabilty bins using Platt scale method
+	/// @param x The training matrix
+	/// @param y The Labels
+	/// @param poly_rank the polynom rank for the Platt scale fit
+	/// @param min_bucket_size The minimal observations to create probabilty bin
+	/// @param min_score_jump The minimal diff in scores to create bin
+	/// </summary>
+	/// <returns>
+	/// @param params Stores the Platt scale model params for conversion
+	/// </returns>
 	int learn_prob_calibration(MedMat<float> &x, vector<float> &y, int poly_rank, vector<double> &params, int min_bucket_size = 10000, float min_score_jump = 0.001);
+	/// <summary>
+	/// Converts probabilty from Platt scale model
+	/// </summary>
 	template<class T, class L> int convert_scores_to_prob(const vector<T> &preds, const vector<double> &params, vector<L> &converted);
 
 	// init
