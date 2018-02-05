33d1316d412472f5e38dae9025a10e7c76236ae8
avi <avi@medial-research.com>
2018-02-05 18:30:10 +0200

    TQRF init, quantization and bagging

--- Internal/TQRF/TQRF/TQRF.cpp
+++ Internal/TQRF/TQRF/TQRF.cpp
@@ -3,3 +3,393 @@
 #define LOCAL_SECTION LOG_MEDALGO
 #define LOCAL_LEVEL	LOG_DEF_LEVEL
 
+//================================================================================================
+// TQRF_Params
+//================================================================================================
+//------------------------------------------------------------------------------------------------
+int TQRF_Params::init(map<string, string>& map)
+{
+	for (auto &f : map) {
+
+		// general
+		if (f.first == "samples_time_unit") samples_time_unit = f.second;
+
+		// time slices
+		else if (f.first == "time_slice_unit") time_slice_unit = f.second;
+		else if (f.first == "time_slice_size") time_slice_size = stoi(f.second);
+		else if (f.first == "time_slices") {
+			vector<string> fields;
+			boost::split(fields, f.second, boost::is_any_of(","));
+			for (auto &ts : fields)
+				time_slices.push_back(stoi(ts));
+		}
+
+		// quantization
+		else if (f.first == "max_q") max_q = stoi(f.second);
+		//else if (f.first == "max_q_sample") max_q_sample = stoi(f.second);
+
+		// trees and stopping criteria
+		if (f.first == "tree_type") tree_type = f.second;
+		else if (f.first == "ntrees") ntrees = stoi(f.second);
+		else if (f.first == "max_depth") max_depth = stoi(f.second);
+		else if (f.first == "min_node_last_slice") min_node_last_slice = stoi(f.second);
+		else if (f.first == "min_node") min_node = stoi(f.second);
+
+		// speedup by subsample control
+		else if (f.first == "max_node_test_samples") max_node_test_samples = stoi(f.second);
+
+		// bagging control
+		else if (f.first == "single_sample_per_pid") single_sample_per_pid = stoi(f.second);
+		else if (f.first == "bag_with_repeats") bag_with_repeats = stoi(f.second);
+		else if (f.first == "bag_prob") bag_prob = stof(f.second);
+		else if (f.first == "bag_ratio") bag_ratio = stof(f.second);
+		else if (f.first == "bag_feat") bag_feat = stof(f.second);
+
+		// missing value
+		else if (f.first == "missing_val") missing_val = stof(f.second);
+		else if (f.first == "missing_method_str") {
+			missing_method_str = f.second;
+			missing_method = TQRF_Forest::get_missing_value_method(missing_method_str);
+		}
+
+		// categorial features
+		else if (f.first == "nvals_for_categorial") nvals_for_categorial = stoi(f.second);
+		else if (f.first == "categorial_str") { boost::split(categorial_str, f.second, boost::is_any_of(",")); }
+		else if (f.first == "categorial_tags") { boost::split(categorial_tags, f.second, boost::is_any_of(",")); }
+
+		// sanities
+		else if (f.first == "test_for_inf") test_for_inf = stoi(f.second);
+		else if (f.first == "test_for_missing") test_for_missing = stoi(f.second);
+
+		// sanities
+		else if (f.first == "verbosity") verbosity = stoi(f.second);
+
+		else {
+			MERR("TQRF_Params::init(): No such parameter \'%s\'\n", f.first.c_str());
+			return -1;
+		}
+	}
+
+	tree_type_i = TQRF_Forest::get_tree_type(tree_type);
+
+	return 0;
+}
+
+//================================================================================================
+// Quantized_Feat
+//================================================================================================
+//------------------------------------------------------------------------------------------------
+// quantize_feat(int i_feat, TQRF_Params &params) :
+// assumes: nfeat, orig_data, ncateg, y are initialized and qx, q_to_val initialized to right size.
+// actually doing the quantization:
+// (1) prepare a value list and sort it.
+// (2) count the number of different values.
+// (3??) in case of categorial and adjacent values with a single same category - unite them. (Not relevant at the moment due to time slicing)
+// (4) if left with less than max_q values : we finished
+// (5) Otherwise: we split given the split strategy (currently - even splits. Future Idea: better subsampling of tails/information rich areas)
+//------------------------------------------------------------------------------------------------
+int Quantized_Feat::quantize_feat(int i_feat, TQRF_Params &params)
+{
+	vector<pair<float,int>> curr_data;
+	vector<float> *data;
+	data = orig_data[i_feat];
+
+	if (params.verbosity > 0) MLOG("Quantized_Feat::quantize_feat :: feat %d %s :: nvals %d\n", i_feat, feat_names[i_feat].c_str(), data->size());
+
+	curr_data.resize(data->size());
+	for (int i=0; i<(int)data->size(); i++)
+		curr_data[i] = { (*data)[i], i };
+
+	// sorting for easier quantization later
+	sort(curr_data.begin(), curr_data.end());
+
+	// count diff values
+	vector<pair<float, int>> v_counts;
+	float prev = curr_data[0].first;
+	int n = 0;
+	for (auto v : curr_data) {
+		if (v.first == prev)
+			n++;
+		else {
+			v_counts.push_back({ prev,n });
+			prev = v.first;
+			n = 1;
+		}
+	}
+	v_counts.push_back({ prev,n }); // last count
+
+	if (params.verbosity > 0) MLOG("Quantized_Feat::quantize_feat :: feat %d %s :: ndiff %d (max_q %d)\n", i_feat, feat_names[i_feat].c_str(), v_counts.size(), params.max_q);
+
+	// Step2 of calculating which features are categorial
+	if (v_counts.size() < params.nvals_for_categorial) is_categorial_feat[i_feat] = 1;
+
+	// prepare q_to_val
+	// the convention is that if a value is in the range (q_to_val[i-1],q_to_val[i]] then its quantized value will be i
+	if (v_counts.size() <= params.max_q) {
+		q_to_val[i_feat].clear();
+		for (auto &v : v_counts)
+			q_to_val[i_feat].push_back(v.first);
+	}
+	else {
+		// need more work to be done, we have too many different values... 
+		// our algorithm is simply trying to make the i-th qval such that there are (i+1)/max_q * len , of the elements
+		// up to some fixes that are caused due to integer numbers
+		int delta = (int)((float)curr_data.size()/(float)params.max_q);
+		int j = 0;
+		int len = (int)curr_data.size();
+		while (j < len-1) {
+			j = j + delta;
+			if (j >= len) j = len - 1;
+			float q = curr_data[j].first;
+			while ((j < len-1) && (curr_data[j+1].first == q)) j++;
+			q_to_val[i_feat].push_back(q);
+		}
+	}
+
+	if (params.verbosity > 1) {
+		MLOG("Quantized_Feat::quantize_feat :: %d %s :: q_to_val size %d :: ", i_feat, feat_names[i_feat].c_str(), q_to_val.size());
+		for (auto q : q_to_val[i_feat])
+			MLOG(" %f :", q);
+		MLOG("\n");
+	}
+
+	// now q_to_val is ready and all that is left is to actually use it to create qx
+	int q_i = 0;
+	int q_size = (int)q_to_val[i_feat].size();
+	qx[i_feat].resize(curr_data.size());
+	for (auto &v : curr_data) {
+		if (q_i < q_size && v.first >= q_to_val[i_feat][q_i])
+			q_i++;
+		qx[i_feat][v.second] = (short)q_i;
+	}
+
+	return 0;
+}
+
+//------------------------------------------------------------------------------------------------
+// Quantized_Feat::init(const MedFeatures &medf, TQRF_Params &params) :
+// initializations needed for all trees together, mainly data quantization , time slices, and
+// filling in all the needed variables in Quantized_Feat
+//------------------------------------------------------------------------------------------------
+int Quantized_Feat::init(MedFeatures &medf, TQRF_Params &params)
+{
+	// filling in needed variables
+	orig_medf = &medf;
+	orig_data.clear();
+	for (auto &df : medf.data) {
+		orig_data.push_back(&df.second);
+	}
+	medf.get_feature_names(feat_names);
+	nfeat = (int)(medf.data.size());
+
+	// init y and get ncateg if needed
+	y.resize(medf.samples.size());
+#pragma omp parallel
+	for (int i=0; i<medf.samples.size(); i++)
+		y[i] = medf.samples[i].outcome;
+
+	if (params.tree_type_i != TQRF_TREE_REGRESSION) {
+		float maxy = *max_element(y.begin(), y.end());
+		ncateg = (int)maxy + 1;
+		// TBD :: sanity test that the y's in this case are all in the 0,ncateg-1 integers range ... currently assuming this
+	}
+
+	// time slices
+	if (params.time_slices.size() == 0) {
+		if (params.time_slice_size > 0)
+			for (int i=0; i<params.n_time_slices; i++)
+				params.time_slices.push_back(i*params.time_slice_size);
+		else
+			params.time_slices.push_back(TQRF_MAX_TIME_SLICE); // single infinite cell
+	}
+	// slices for each sample
+	// we assume that '0' outcome is the control, for binary and multicategory cases
+	// The regression case for time slices is much more complex, and needs more input from the user: currently a project to the future
+	last_time_slice.resize(medf.samples.size());
+#pragma omp parallel
+	for (int i=0; i<medf.samples.size(); i++) {
+		for (int j=0; j<params.time_slices.size(); j++)
+			if (medf.samples[i].outcomeTime < params.time_slices[j]) {
+				last_time_slice[i] = j;
+				break;
+			}
+	}
+
+	// categorial features Step1 (Step2 will be done in quantize_feat)
+	is_categorial_feat.clear();
+	is_categorial_feat.resize(nfeat, 0);
+	for (int i=0; i<nfeat; i++) {
+		for (int j=0; j<params.categorial_str.size(); j++) {
+			if (feat_names[i].find(params.categorial_str[j]) != string::npos) {
+				is_categorial_feat[i] = 1;
+				break;
+			}
+		}
+
+		for (int j=0; j<params.categorial_tags.size(); j++) {
+			if (medf.tags[feat_names[i]].find(params.categorial_tags[j]) != medf.tags[feat_names[i]].end()) {
+				is_categorial_feat[i] = 1;
+				break;
+			}
+		}
+	}	
+
+	int rc = 0;
+	// quantization of features
+#pragma omp parallel
+	for (int i=0; i<nfeat; i++)
+		if (quantize_feat(i, params) < 0)
+			rc = -1;
+
+	// rc
+	return rc;
+}
+
+
+//================================================================================================
+// TQRF_Forest
+//================================================================================================
+//------------------------------------------------------------------------------------------------
+int TQRF_Forest::get_tree_type(const string &str)
+{
+	if (boost::iequals(str, "entropy")) return TQRF_TREE_ENTROPY;
+	if (boost::iequals(str, "logrank")) return TQRF_TREE_LOGRANK;
+	if (boost::iequals(str, "regression")) return TQRF_TREE_REGRESSION;
+
+	return TQRF_TREE_UNDEFINED;
+}
+
+//------------------------------------------------------------------------------------------------
+int TQRF_Forest::get_missing_value_method(const string &str)
+{
+	if (boost::iequals(str, "mean")) return TQRF_MISSING_VALUE_MEAN;
+	if (boost::iequals(str, "median")) return TQRF_MISSING_VALUE_MEDIAN;
+	if (boost::iequals(str, "larger")) return TQRF_MISSING_VALUE_LARGER_NODE;
+	if (boost::iequals(str, "left")) return TQRF_MISSING_VALUE_LEFT;
+	if (boost::iequals(str, "right")) return TQRF_MISSING_VALUE_RIGHT;
+	if (boost::iequals(str, "rand")) return TQRF_MISSING_VALUE_RAND_ALL;
+	if (boost::iequals(str, "rand_each_sample")) return TQRF_MISSING_VALUE_RAND_EACH_SAMPLE;
+
+	return TQRF_MISSING_VALUE_NOTHING;
+}
+
+
+//================================================================================================
+// TQRF_Tree
+//================================================================================================
+//--------------------------------------------------------------------------------------------------------------------
+int TQRF_Tree::get_bagged_indexes()
+{
+	unordered_set<int> s_pids[2] , s_indexes[2];
+	vector<int> v_pids[2], v_indexes[2];
+	unordered_map<int, vector<int>> pid2indexes;
+
+	bool is_regression = (tree_type != TQRF_TREE_REGRESSION);
+
+	for (int i=0; i<_qfeat.orig_medf->samples.size(); i++) {
+		MedSample &s = _qfeat.orig_medf->samples[i];
+		int j = 0;
+		if (is_regression || (s.outcome != 0)) j=1;
+
+		if (_params.single_sample_per_pid) {
+			if (s_pids[j].find(s.id) == s_pids[j].end()) { v_pids[j].push_back(s.id); s_pids[j].insert(s.id); }
+			if (pid2indexes.find(s.id) == pid2indexes.end()) pid2indexes[s.id] = vector<int>();
+			pid2indexes[s.id].push_back(i);
+		}
+		else {
+			if (s_indexes[j].find(s.id) == s_indexes[j].end()) { v_indexes[j].push_back(i); s_indexes[j].insert(i); }
+		}
+	}
+
+	if (tree_type != TQRF_TREE_REGRESSION) {
+
+		// calculate the bagging probabilities for 0 and 1
+		int n0, n1;
+		float p0, p1;
+
+		if (_params.bag_ratio < 0) {
+			p0 = _params.bag_prob;
+			p1 = _params.bag_prob;
+		}
+		else {
+			if (_params.single_sample_per_pid) {
+				n0 = (int)s_pids[0].size();
+				n1 = (int)s_pids[1].size();
+			}
+			else {
+				n0 = (int)s_indexes[0].size();
+				n1 = (int)s_indexes[1].size();
+			}
+
+			//
+			// following calculations use the fact that we want : bag_ratio = (n0*p0)/(n1*p1)
+			//
+			if (n0 > n1) {
+				p1 = _params.bag_prob;
+				p0 = _params.bag_ratio * (float)(n1+1) * p1 / (float)(n0+1);
+			}
+			else {
+				p0 = _params.bag_prob;
+				p1 = p0 * (float)(n0+1) / (_params.bag_ratio * (float)(n1+1));
+			}
+
+			p0 = min(p0, (float)1);
+			p1 = min(p1, (float)1);
+		}
+
+		indexes.clear();
+
+		bag_chooser(_params.bag_with_repeats, _params.single_sample_per_pid, p0, v_pids[0], v_indexes[0], pid2indexes, indexes);
+		bag_chooser(_params.bag_with_repeats, _params.single_sample_per_pid, p1, v_pids[1], v_indexes[1], pid2indexes, indexes);
+
+	}
+	else {
+		// in regression all samples are with j==1
+		bag_chooser(_params.bag_with_repeats, _params.single_sample_per_pid, _params.bag_prob, v_pids[1], v_indexes[1], pid2indexes, indexes);
+	}
+
+}
+
+//--------------------------------------------------------------------------------------------------------------------
+int TQRF_Tree::bag_chooser(int choose_with_repeats, int single_sample_per_id, float p, vector<int> &pids, vector<int> &idx, unordered_map<int, vector<int>> &pid2idx, /* OUT APPEND */ vector<int> &_indexes)
+{
+	int n_pids = (int)pids.size();
+	int n_idx = (int)idx.size();
+
+	if (single_sample_per_id) {
+
+		if (choose_with_repeats) {
+			unordered_map<int, int> pid_idx;
+			int n_choose = (int)(p*(float)n_pids);
+			for (int i=0; i<n_choose; i++) {
+				int rand_pid = pids[rand_N(n_pids)];
+				if (pid_idx.find(rand_pid) == pid_idx.end()) {
+					int len_pid = (int)pid2idx[rand_pid].size();
+					int rand_idx = rand_N(len_pid);
+					pid_idx[rand_pid] = pid2idx[rand_pid][rand_idx];
+				}
+				_indexes.push_back(pid_idx[rand_pid]); // making sure to choose the SAME sample per id
+			}
+		}
+		else {
+			for (int i=0; i<n_pids; i++)
+				if (rand_1() < p)
+					_indexes.push_back(pid2idx[pids[i]][rand_N((int)pid2idx[pids[i]].size())]);
+		}
+
+	}
+	else {
+		if (choose_with_repeats) {
+
+			int n_choose = (int)(p*(float)n_idx);
+			for (int i=0; i<n_choose; i++)
+				_indexes.push_back(idx[rand_N(n_idx)]);
+
+		}
+		else {
+			for (int i=0; i<n_idx; i++)
+				if (rand_1() < p)
+					_indexes.push_back(idx[i]);
+		}
+	}
+}
--- Internal/TQRF/TQRF/TQRF.h
+++ Internal/TQRF/TQRF/TQRF.h
@@ -6,10 +6,33 @@
 #include <Logger/Logger/Logger.h>
 #include <MedProcessTools/MedProcessTools/SerializableObject.h>
 #include <MedProcessTools/MedProcessTools/MedSamples.h>
+#include <MedProcessTools/MedProcessTools/MedFeatures.h>
 #include <MedUtils/MedUtils/MedMat.h>
 #include <MedTime/MedTime/MedTime.h>
+#include <queue>
 
 using namespace std;
+
+enum TQRF_TreeTypes {
+	TQRF_TREE_ENTROPY = 0,
+	TQRF_TREE_REGRESSION = 1,
+	TQRF_TREE_LOGRANK = 3,
+	TQRF_TREE_UNDEFINED = 4
+};
+
+enum TQRF_Missing_Value_Method {
+	TQRF_MISSING_VALUE_MEAN = 0,
+	TQRF_MISSING_VALUE_MEDIAN = 1,
+	TQRF_MISSING_VALUE_LARGER_NODE = 2,
+	TQRF_MISSING_VALUE_LEFT = 3,
+	TQRF_MISSING_VALUE_RIGHT = 4,
+	TQRF_MISSING_VALUE_RAND_ALL = 5,
+	TQRF_MISSING_VALUE_RAND_EACH_SAMPLE = 6,
+	TQRF_MISSING_VALUE_NOTHING = 7
+};
+
+#define TQRF_MAX_TIME_SLICE			10000000
+
 
 //==========================================================================================================================
 class TQRF_Params : SerializableObject {
@@ -18,20 +41,22 @@ public:
 	//========================================================================================================================
 	// params list
 	//========================================================================================================================
-	string init_string = "";			/// sometimes help to keep it for debugging
+	string init_string = "";			/// sometimes it helps to keep it for debugging
 	string samples_time_unit = "Date";
 	
 	// time slices
 	string time_slice_unit = "Days";
 	int time_slice_size = -1;			/// the size of the basic time slice, -1: is like infinity: a single time slice like a regular QRF
+	int n_time_slices = 1;				/// if time_slices vector is not given, one will be created using time_slice_size and this parameter. 
 	vector<int> time_slices = {};		/// if not empty: defines the borders of all the time lines. Enables a very flexible time slicing strategy
 	
 	// quantization
 	int max_q = 200;					/// maximal quantization
-	int max_q_sample = 100000;			/// the max number of values to use when deciding q limits
+	//int max_q_sample = 100000;			/// the max number of values to use when deciding q limits
 
 	// trees and stopping criteria
 	string tree_type = "";				/// options: regression, entropy, logrank
+	int tree_type_i = -1;				/// tree type code : calulated from tree_type the string
 	int	ntrees = 50;					/// number of trees to learn
 	int max_depth = 100;				/// maximal depth of tree
 	int min_node_last_slice = 10;		/// stopping criteria : minimal number of samples in a node in the last time slice
@@ -39,7 +64,7 @@ public:
 
 	// feature sampling
 	int ntry = -1;						/// -1: use the ntry_prob rule, > 0 : choose this number of features.
-	float ntry_prob = 0.1;				/// choose ntry_prob * nfeat features each time
+	float ntry_prob = (float)0.1;		/// choose ntry_prob * nfeat features each time
 	int nsplits = -1;					/// -1: check all splits for each feature , then split the max, > 0: choose this number of split points at random and choose best
 
 	// speedup by subsample control
@@ -48,8 +73,15 @@ public:
 	// bagging control
 	int single_sample_per_pid = 1;		/// when bagging select a single sample per pid (which in itself can be repeated)
 	int bag_with_repeats = 1;			/// weather to bag with repeats or not
-	float bag_prob = 0.5;				/// random choice of samples for each tree prob
+	float bag_prob = (float)0.5;		/// random choice of samples for each tree prob
 	float bag_ratio = -1;				/// control ratio of #0 : #NonZero of labels, if < -1 , leave as is.
+	float bag_feat = (float)1.0;		/// proportion of random features chosen for each tree
+
+	// categorial featues
+	int nvals_for_categorial = 0;			/// features with number of different values below nvals_for_categ will be assumed categorial
+	vector<string> categorial_str;		/// all features containing one of the strings defined here in their name will be assumed categorial
+	vector<string> categorial_tags;		/// all features containing these tags will be assumed categorial
+	//vector<int> categorial;				/// calculated from the above (in learning, once train data is given. In testing - already ready)
 
 	// missing value
 	float missing_val = MED_MAT_MISSING_VALUE;	/// missing value
@@ -59,14 +91,48 @@ public:
 	// sanities
 	int test_for_inf = 1;				/// will fail on non finite values in input data	
 	int test_for_missing = 0;			/// will fail on if missing value found in data
+
+	// verbosity
+	int verbosity = 0;					/// for debug prints
+
 	//========================================================================================================================
 
 
 	/// initialization from string
+	int init(map<string, string>& map);
 
 	// Serialization
 	//ADD_SERIALIZATION_FUNCS(init_string, samples_time_unit, time_slice_unit, time_slice_size, time_slices, max_q, max_q_sample, tree_type, ntrees, max_depth, min_node_last_slice, min_node, )
 };
+
+
+//==========================================================================================================================
+// contains all the needed data for training including all quantizations (features, time slices) that are needed
+//==========================================================================================================================
+class Quantized_Feat : SerializableObject {
+
+public:
+	vector<vector<short>> qx;		/// a vector of features that mimics the input x_in features matrix, but coded into quantized values
+	vector<vector<float>> q_to_val; /// from a q value to float value : q=0 is reserved for missing value
+									/// the range for q>0 is : [q_to_val[q], q_to_val[q+1])
+	MedFeatures *orig_medf;			   /// pointer to the original MedFeatures
+
+	int nfeat = 0;					/// just an easy helper that = qx.size()
+	int ncateg = 0;					/// ncateg 0 is regression, otherwise categories are assumed to be 0 ... ncateg-1
+	vector<vector<float> *> orig_data; /// pointers to the original data given
+	vector<string> feat_names;		   /// as given in train
+	vector<float> y;
+	vector<int> last_time_slice;	/// when there's more than 1 time slice there may be censoring involved and the last_time_slice is the last uncensored one.
+	int n_time_slices;				/// 1 time slice is simply the regular case of a label for the whole future
+	vector<int> is_categorial_feat;
+
+	int init(MedFeatures &medf, TQRF_Params &params);
+
+private:
+	int quantize_feat(int i_feat, TQRF_Params &params);
+
+};
+
 
 //==========================================================================================================================
 // a basic node class : currently a single node type serves all trees .... could be changed to 
@@ -109,7 +175,7 @@ public:
 // for categorial outcomes:
 // for each time slot, for each quanta -> counts for each category
 // for regression outcomes:
-// for each time slit, for each quanta -> nvals and sum (??)
+// for each time slot, for each quanta -> nvals and sum (??)
 //==========================================================================================================================
 class TQRF_Split_Stat {
 
@@ -121,10 +187,12 @@ class TQRF_Split_Stat {
 class TQRF_Tree : SerializableObject {
 
 public:
-	int TQRF_Tree_Type;
+	int tree_type;
+	int id;						// for debug prints - a specific tree identifier
 	int keep_indexes = 0;
 	vector<int> indexes;		// indexes[i] = an index of a sample in the given Quantized_Feat
 	vector<TQRF_Node> nodes;	// this node supports currently all possible nodes for all trees... to save ugly templated code
+	vector<int> i_feats;		// feature indexes to be used in this tree (they can be bagged as well)
 
 	void init(Quantized_Feat &qfeat, TQRF_Params &params) { _qfeat = qfeat; _params = params; }
 	int Train(Quantized_Feat &qfeat, TQRF_Params &params) {	init(qfeat, params); return Train(); }
@@ -140,40 +208,27 @@ public:
 	// Step(3) : train and add new nodes as long as needed
 	int train_node(int i_node);
 
-	// 
+	// within train_node()
 
+	// Step(3.1) : get the relevant histograms 
+	int get_feature_stat();
 
 	// static TQRF_Tree* make_tree(const string &type);
 
 private:
 	Quantized_Feat &_qfeat;
 	TQRF_Params &_params;
+
+	// next used to manage nodes while building
+	int n_nodes_in_process = 0;
+	int i_last_node_in_process = 0;
+
+	int bag_chooser(int choose_with_repeats, int single_sample_per_id, float p, vector<int> &pids, vector<int> &idx, unordered_map<int, vector<int>> &pid2idx, /* OUT APPEND */ vector<int> &_indexes);
+
+
 };
 
 
-//==========================================================================================================================
-// contains all the needed data for training including all quantizations (features, time slices) that are needed
-//==========================================================================================================================
-class Quantized_Feat : SerializableObject {
-
-public:
-	vector<vector<short>> qx;		/// a vector of features that mimics the input x_in features matrix, but coded into quantized values
-	vector<vector<float>> q_to_val; /// from a q value to float value : q=0 is reserved for missing value
-									/// the range for q>0 is : [q_to_val[q], q_to_val[q+1])
-	int nfeat = 0;					/// just an easy helper that = qfeat.size()
-	vector<float> y;
-	vector<float> w;
-	vector<MedSample> samples;
-	vector<int> last_time_slice;
-	int n_time_slices;
-	vector<int> is_categorial_feat;
-
-	int init(const MedFeatures &medf, TQRF_Params &params);
-
-private:
-	int quantize_feat(const MedMat<float> &x_in, int i_feat, TQRF_Params &params);
-
-};
 
 //==========================================================================================================================
 class TQRF_Forest : SerializableObject {
@@ -181,19 +236,24 @@ class TQRF_Forest : SerializableObject {
 public:
 
 	TQRF_Params params;
-	vector<TQRF_Tree *> trees;
+	vector<TQRF_Tree> trees;
 
 	/// The basic train matrix for TQRF is MedFeatures (!!) the reason is that it contains everything in one place:
 	/// that is: the X features, the Y outcome, the weights and the samples for each row.
 	/// All of these are needed when calculating a logrank score for example
-	int Train(const MedFeatures &medf);
+	/// The y matrix is added since we may want to use regression with y values given for every time slice ...
+	int Train(const MedFeatures &medf, const MedMat<float> &Y);
+	int Train(const MedFeatures &medf) { MedMat<float> dummy; return Train(medf, dummy); }
 
 
 
+	// simple helpers
+	static int get_tree_type(const string &str);
+	static int get_missing_value_method(const string &str);
 private:
 
 };
 
 //========================================
-// Join the serialization Waggaon
+// Join the serialization Waggon
 //========================================
