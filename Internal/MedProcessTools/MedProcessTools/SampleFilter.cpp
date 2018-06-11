666b4dfb135f039da28f727bde2247b34aeaaa95
avi <avi@medial-research.com>
2018-06-11 16:01:45 +0300

    debug print

--- Internal/MedProcessTools/MedProcessTools/SampleFilter.cpp
+++ Internal/MedProcessTools/MedProcessTools/SampleFilter.cpp
@@ -1204,6 +1204,7 @@ int SanitySimpleFilter::test_filter(MedSample &sample, MedRepository &rep, int &
 #endif
 
 					if (values_in_dictionary && section_id > 0) {
+						//MLOG("dictionary test: section_id %d sig_name %s i_val %f dsize %d\n", section_id, sig_name.c_str(), i_val, rep.dict.dicts[section_id].Id2Name.size());
 						if (rep.dict.dicts[section_id].Id2Name.find((int)i_val) == rep.dict.dicts[section_id].Id2Name.end())
 							n_not_in_dict++;
 					}
