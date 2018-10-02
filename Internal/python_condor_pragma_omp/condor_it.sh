ab191016114cec708992f84e648745aff79f8c86
ido hadanny <ido@medial-research.com>
2018-10-02 17:34:13 +0300

    in the condor submit file, add getenv = True. For some reason, the default is false, and as we have some important env variables  (LD_LIBRARY_PATH), it's best to change it.

--- Internal/python_condor_pragma_omp/condor_it.sh
+++ Internal/python_condor_pragma_omp/condor_it.sh
@@ -5,10 +5,13 @@ set -o pipefail # makes sure a bad exit code is returned when appropriate
 
 export OMP_NUM_THREADS=7
 export PATH
+#export LD_LIBRARY_PATH=/server/Work/Libs/Boost/latest/stage/lib
 echo "hostname:" `hostname`
 echo "whoami:" `whoami`
 echo "SHELL: $SHELL"
 echo "PATH: $PATH"
+echo "MR_ROOT: [$MR_ROOT]"
+echo "LD_LIBRARY_PATH: [$LD_LIBRARY_PATH]"
 echo "which python: " `which python`
 echo "python --version: " 
 python --version
