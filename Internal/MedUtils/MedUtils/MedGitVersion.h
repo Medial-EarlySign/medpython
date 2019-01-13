c0cd3142a9137a395c94d542943f23bde881c812
Alon Lanyado <alon@medial-research.com>
2019-01-13 17:18:51 +0200

    MedGitVersion: update

--- Internal/MedUtils/MedUtils/MedGitVersion.h
+++ Internal/MedUtils/MedUtils/MedGitVersion.h
@@ -2,7 +2,10 @@
 // support "--version" of our tools
 #ifndef __MED_GIT_VERSION_H__
 #define __MED_GIT_VERSION_H__
+
+#ifndef GIT_HEAD_VERSION
 #define GIT_HEAD_VERSION "not built using scripts - please define GIT_HEAD_VERSION in compilation"
+#endif
 
 #endif // !__MED_GIT_VERSION_H__
 
