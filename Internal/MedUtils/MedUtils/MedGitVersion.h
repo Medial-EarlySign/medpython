1474f542f33de4bbc2fdf00af5198b01fcf605d7
Alon Lanyado <alon@medial-research.com>
2019-01-13 14:30:12 +0200

    add default support for version in all our programs when using build script

---
+++ Internal/MedUtils/MedUtils/MedGitVersion.h
@@ -0,0 +1,8 @@
+//This File contains information about build of tools - put here git head commit version and date to 
+// support "--version" of our tools
+#ifndef __MED_GIT_VERSION_H__
+#define __MED_GIT_VERSION_H__
+#define GIT_HEAD_VERSION "not built using scripts - please define GIT_HEAD_VERSION in compilation"
+
+#endif // !__MED_GIT_VERSION_H__
+
