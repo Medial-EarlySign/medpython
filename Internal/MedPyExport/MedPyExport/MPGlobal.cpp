03391cbb7111bc7e19eca13934d0fd26e4974757
Shlomi Loubaton <shlomi@medial-research.com>
2019-02-26 15:45:35 +0200

    Global api object

---
+++ Internal/MedPyExport/MedPyExport/MPGlobal.cpp
@@ -0,0 +1,13 @@
+#include "MPGlobal.h"
+#include "MedTime/MedTime/MedTime.h"
+#include "MedUtils/MedUtils/MedGlobalRNG.h"
+
+int MPGlobalClass::MEDPY_GET_default_time_unit() { return global_default_time_unit; }
+void MPGlobalClass::MEDPY_SET_default_time_unit(int new_val) { global_default_time_unit = new_val; }
+int MPGlobalClass::MEDPY_GET_default_windows_time_unit() { return global_default_windows_time_unit; }
+void MPGlobalClass::MEDPY_SET_default_windows_time_unit(int new_val) { global_default_windows_time_unit = new_val; }
+
+int MPRNG::rand() { return globalRNG::rand(); };
+int MPRNG::rand30() { return globalRNG::rand30(); };
+void MPRNG::srand(int val) { globalRNG::srand(val); };
+int MPRNG::max() { return globalRNG::max(); };
