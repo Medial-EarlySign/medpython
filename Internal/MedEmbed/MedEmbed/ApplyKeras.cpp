0ccc259581cc3ea226ba677db3ed795590122913
yaron kinar <yaron@medial-research.com>
2019-03-27 14:19:59 +0200

    fix non-sparse apply in ApplyKeras

--- Internal/MedEmbed/MedEmbed/ApplyKeras.cpp
+++ Internal/MedEmbed/MedEmbed/ApplyKeras.cpp
@@ -257,9 +257,10 @@ int ApplyKeras::apply(vector<float>& line, vector<float> &output, int to_layer)
 {
 	if (to_layer < 0) to_layer = (int)layers.size() - 1 + to_layer;
 
-	vector<vector<float>> outs(layers.size(), vector<float>());
-	for (int i = 0; i <= to_layer; i++) {
+	vector<vector<float>> outs(to_layer+1, vector<float>());
+	layers[0].apply(line, outs[0]);
 
+	for (int i = 1; i <= to_layer; i++) {
 		//MLOG("apply_sparse layer %d (passed %d) to_layer=%d (name %s type %d activation %d)\n", i, passed_first_dense, to_layer, layers[i].name.c_str(), layers[i].type, layers[i].activation);
 		layers[i].apply(outs[i - 1], outs[i]);
 		//MLOG("out[i] size %d\n", outs[i].size());
