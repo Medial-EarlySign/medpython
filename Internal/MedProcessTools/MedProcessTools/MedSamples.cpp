50841ca4a6e63f3e01756e1da3e1705b2abeeb2e
Alon Lanyado <alon@medial-research.com>
2019-04-28 15:34:53 +0300

    MedSamples: remove wrong warning from reading header

--- Internal/MedProcessTools/MedProcessTools/MedSamples.cpp
+++ Internal/MedProcessTools/MedProcessTools/MedSamples.cpp
@@ -319,6 +319,7 @@ int extract_field_pos_from_header(vector<string> field_names, map <string, int> 
 			string attr_name = field_names[i].substr(9, field_names[i].length() - 9);
 			str_attr_pos[attr_name] = i;
 		}
+		else if (i == 0 && field_names[i] == "EVENT_FIELDS") {}
 		else unknown_fields.push_back(field_names[i]);
 	}
 	if (unknown_fields.size() > 0) {
