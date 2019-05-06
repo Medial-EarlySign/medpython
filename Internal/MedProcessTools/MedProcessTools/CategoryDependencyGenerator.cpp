4528622b4f76d414187337db8e45b094b560d349
yaron kinar <yaron@medial-research.com>
2019-05-06 17:25:07 +0300

    fix a threading bug in CategoryDependenceGenerator

--- Internal/MedProcessTools/MedProcessTools/CategoryDependencyGenerator.cpp
+++ Internal/MedProcessTools/MedProcessTools/CategoryDependencyGenerator.cpp
@@ -168,8 +168,16 @@ int _count_legal_rows(const  vector<vector<int>> &m, int minimal_balls) {
 }
 
 void CategoryDependencyGenerator::get_parents(int codeGroup, vector<int> &parents, const regex &reg_pat) {
-	if (_member2Sets_flat_cache.find(codeGroup) != _member2Sets_flat_cache.end())
+	
+	bool cached = false;
+#pragma omp critical
+	if (_member2Sets_flat_cache.find(codeGroup) != _member2Sets_flat_cache.end()) {
 		parents = _member2Sets_flat_cache.at(codeGroup);
+		cached = true;
+	}
+	if (cached)
+		return;
+
 	vector<int> last_parents = { codeGroup };
 	if (last_parents.front() < 0)
 		return; //no parents
