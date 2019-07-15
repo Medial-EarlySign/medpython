f720b30e3647b7599caa599d7be51328be994aec
Shlomi Loubaton <shlomi@medial-research.com>
2019-07-15 16:03:12 +0300

    DynAMWrapper to expose function pointers and support multiple AMs

--- Internal/AlgoMarker/DynAMWrapper/DynAMWrapper.cpp
+++ Internal/AlgoMarker/DynAMWrapper/DynAMWrapper.cpp
@@ -9,60 +9,10 @@
 #endif // linux/win
 
 #include "DynAMWrapper.h"
-//#include "AlgoMarkerFlat.h"
 
-class so_functions {
-public:
-  typedef int (*t_AM_API_Create)(int am_type, AlgoMarker **new_am);
-  typedef int (*t_AM_API_Load)(AlgoMarker * pAlgoMarker, const char *config_fname);
-  typedef int (*t_AM_API_ClearData)(AlgoMarker * pAlgoMarker);
-  typedef int (*t_AM_API_AddData)(AlgoMarker * pAlgoMarker, int patient_id, const char *signalName, int TimeStamps_len, long long* TimeStamps, int Values_len, float* Values);
-  typedef int (*t_AM_API_AddDataStr)(AlgoMarker * pAlgoMarker, int patient_id, const char *signalName, int TimeStamps_len, long long* TimeStamps, int Values_len, char** Values);
-  typedef int (*t_AM_API_CreateRequest)(char *requestId, char **score_types, int n_score_types, int *patient_ids, long long *time_stamps, int n_points, AMRequest **new_req);
-  typedef int (*t_AM_API_CreateResponses)(AMResponses **);
-  typedef int (*t_AM_API_Calculate)(AlgoMarker *pAlgoMarker, AMRequest *request, AMResponses *responses);
-  typedef int (*t_AM_API_GetSharedMessages)(AMResponses *responses, int *n_msgs, int **msgs_codes, char ***msgs_args);
-  typedef int (*t_AM_API_GetResponsesNum)(AMResponses *responses);
-  typedef int (*t_AM_API_GetResponseIndex)(AMResponses *responses, int _pid, long long _timestamp);
-  typedef int (*t_AM_API_GetResponsesRequestId)(AMResponses *responses, char **requestId);
-  typedef int (*t_AM_API_GetResponseScoreByType)(AMResponses *responses, int res_index, char *_score_type, float *out_score);
-  typedef int (*t_AM_API_GetResponseAtIndex)(AMResponses *responses, int index, AMResponse **response);
-  typedef int (*t_AM_API_GetResponseScoresNum)(AMResponse *response, int *n_scores);
-  typedef int (*t_AM_API_GetResponseScoreByIndex)(AMResponse *response, int score_index, float *score, char **_score_type);
-  typedef int (*t_AM_API_GetResponseMessages)(AMResponse *response, int *n_msgs, int **msgs_codes, char ***msgs_args);
-  typedef int (*t_AM_API_GetScoreMessages)(AMResponse *response, int score_index, int *n_msgs, int **msgs_codes, char ***msgs_args);
-  typedef int (*t_AM_API_GetResponsePoint)(AMResponse *response, int *pid, long long *timestamp);
-  typedef int (*t_AM_API_GetName)(AlgoMarker * pAlgoMArker, char **name);
-  typedef void (*t_AM_API_DisposeAlgoMarker)(AlgoMarker*);
-  typedef void (*t_AM_API_DisposeResponses)(AMResponses*);
-  typedef void (*t_AM_API_DisposeRequest)(AMRequest*);
-  void *AM_API_Create=nullptr;
-  void *AM_API_Load=nullptr;
-  void *AM_API_ClearData=nullptr;
-  void *AM_API_AddData=nullptr;
-  void *AM_API_AddDataStr = nullptr;
-  void *AM_API_CreateRequest=nullptr;
-  void *AM_API_CreateResponses=nullptr;
-  void *AM_API_Calculate=nullptr;
-  void *AM_API_GetSharedMessages=nullptr;
-  void *AM_API_GetResponsesNum=nullptr;
-  void *AM_API_GetResponseIndex=nullptr;
-  void *AM_API_GetResponsesRequestId=nullptr;
-  void *AM_API_GetResponseScoreByType=nullptr;
-  void *AM_API_GetResponseAtIndex=nullptr;
-  void *AM_API_GetResponseScoresNum=nullptr;
-  void *AM_API_GetResponseScoreByIndex=nullptr;
-  void *AM_API_GetResponseMessages=nullptr;
-  void *AM_API_GetScoreMessages=nullptr;
-  void *AM_API_GetResponsePoint=nullptr;
-  void *AM_API_GetName=nullptr;
-  void *AM_API_DisposeAlgoMarker=nullptr;
-  void *AM_API_DisposeResponses=nullptr;
-  void *AM_API_DisposeRequest=nullptr;
-};
+so_functions* so_functions::so = nullptr;
+std::vector<so_functions> so_functions::sos;
 
-
-so_functions so;
 
 void* load_sym(void* lib_h, const char* sym_name, bool exit_on_fail=true)
 {
@@ -85,6 +35,11 @@ void* load_sym(void* lib_h, const char* sym_name, bool exit_on_fail=true)
 }
 
 void load_am(const char * am_fname){
+  if(so_functions::load(am_fname)<0)
+    exit(0);
+}
+
+int so_functions::load(const char * am_fname){
   printf("Loading %s ... ",am_fname);
 #ifdef __linux__ 
   void* lib_handle = dlopen(am_fname, RTLD_NOW); //RTLD_LAZY
@@ -100,139 +55,142 @@ void load_am(const char * am_fname){
 #elif _WIN32
 	  printf("Failed loading %s\n", am_fname);
 #endif	
-    exit(0);
+    return -1;
   }
+  sos.push_back(so_functions());
+  so = &sos.back();
   printf("OK\n");
-  so.AM_API_Create = load_sym(lib_handle, "AM_API_Create");
-  so.AM_API_Load = load_sym(lib_handle, "AM_API_Load");
-  so.AM_API_ClearData = load_sym(lib_handle, "AM_API_ClearData");
-  so.AM_API_AddData = load_sym(lib_handle, "AM_API_AddData");
-  so.AM_API_AddDataStr = load_sym(lib_handle, "AM_API_AddDataStr", false);
-  so.AM_API_CreateRequest = load_sym(lib_handle, "AM_API_CreateRequest");
-  so.AM_API_CreateResponses = load_sym(lib_handle, "AM_API_CreateResponses");
-  so.AM_API_Calculate = load_sym(lib_handle, "AM_API_Calculate");
-  so.AM_API_GetResponsesNum = load_sym(lib_handle, "AM_API_GetResponsesNum");
-  so.AM_API_GetSharedMessages = load_sym(lib_handle, "AM_API_GetSharedMessages");
-  so.AM_API_GetResponseIndex = load_sym(lib_handle, "AM_API_GetResponseIndex");
-  so.AM_API_GetResponsesRequestId = load_sym(lib_handle, "AM_API_GetResponsesRequestId");
-  so.AM_API_GetResponseScoreByType = load_sym(lib_handle, "AM_API_GetResponseScoreByType");
-  so.AM_API_GetResponseAtIndex = load_sym(lib_handle, "AM_API_GetResponseAtIndex");
-  so.AM_API_GetResponseScoresNum = load_sym(lib_handle, "AM_API_GetResponseScoresNum");
-  so.AM_API_GetResponseScoreByIndex = load_sym(lib_handle, "AM_API_GetResponseScoreByIndex");
-  so.AM_API_GetResponseMessages = load_sym(lib_handle, "AM_API_GetResponseMessages");
-  so.AM_API_GetScoreMessages = load_sym(lib_handle, "AM_API_GetScoreMessages");
-  so.AM_API_GetResponsePoint = load_sym(lib_handle, "AM_API_GetResponsePoint");
-  so.AM_API_GetName = load_sym(lib_handle, "AM_API_GetName");
-  so.AM_API_DisposeAlgoMarker = load_sym(lib_handle, "AM_API_DisposeAlgoMarker");
-  so.AM_API_DisposeRequest = load_sym(lib_handle, "AM_API_DisposeRequest");
-  so.AM_API_DisposeResponses = load_sym(lib_handle, "AM_API_DisposeResponses");
+  so->AM_API_Create = load_sym(lib_handle, "AM_API_Create");
+  so->AM_API_Load = load_sym(lib_handle, "AM_API_Load");
+  so->AM_API_ClearData = load_sym(lib_handle, "AM_API_ClearData");
+  so->AM_API_AddData = load_sym(lib_handle, "AM_API_AddData");
+  so->AM_API_AddDataStr = load_sym(lib_handle, "AM_API_AddDataStr", false);
+  so->AM_API_CreateRequest = load_sym(lib_handle, "AM_API_CreateRequest");
+  so->AM_API_CreateResponses = load_sym(lib_handle, "AM_API_CreateResponses");
+  so->AM_API_Calculate = load_sym(lib_handle, "AM_API_Calculate");
+  so->AM_API_GetResponsesNum = load_sym(lib_handle, "AM_API_GetResponsesNum");
+  so->AM_API_GetSharedMessages = load_sym(lib_handle, "AM_API_GetSharedMessages");
+  so->AM_API_GetResponseIndex = load_sym(lib_handle, "AM_API_GetResponseIndex");
+  so->AM_API_GetResponsesRequestId = load_sym(lib_handle, "AM_API_GetResponsesRequestId");
+  so->AM_API_GetResponseScoreByType = load_sym(lib_handle, "AM_API_GetResponseScoreByType");
+  so->AM_API_GetResponseAtIndex = load_sym(lib_handle, "AM_API_GetResponseAtIndex");
+  so->AM_API_GetResponseScoresNum = load_sym(lib_handle, "AM_API_GetResponseScoresNum");
+  so->AM_API_GetResponseScoreByIndex = load_sym(lib_handle, "AM_API_GetResponseScoreByIndex");
+  so->AM_API_GetResponseMessages = load_sym(lib_handle, "AM_API_GetResponseMessages");
+  so->AM_API_GetScoreMessages = load_sym(lib_handle, "AM_API_GetScoreMessages");
+  so->AM_API_GetResponsePoint = load_sym(lib_handle, "AM_API_GetResponsePoint");
+  so->AM_API_GetName = load_sym(lib_handle, "AM_API_GetName");
+  so->AM_API_DisposeAlgoMarker = load_sym(lib_handle, "AM_API_DisposeAlgoMarker");
+  so->AM_API_DisposeRequest = load_sym(lib_handle, "AM_API_DisposeRequest");
+  so->AM_API_DisposeResponses = load_sym(lib_handle, "AM_API_DisposeResponses");
+  return (int)sos.size()-1;
 }
 
 int AM_API_ClearData(AlgoMarker * pAlgoMarker){
-  return (*((so_functions::t_AM_API_ClearData)so.AM_API_ClearData))
+  return (*((so_functions::t_AM_API_ClearData)so_functions::so->AM_API_ClearData))
     (pAlgoMarker);
 }
 
 void AM_API_DisposeAlgoMarker(AlgoMarker * pAlgoMarker){
-  (*((so_functions::t_AM_API_DisposeAlgoMarker)so.AM_API_DisposeAlgoMarker))
+  (*((so_functions::t_AM_API_DisposeAlgoMarker)so_functions::so->AM_API_DisposeAlgoMarker))
     (pAlgoMarker);
 }
 
 void AM_API_DisposeRequest(AMRequest *pRequest){
-  (*((so_functions::t_AM_API_DisposeRequest)so.AM_API_DisposeRequest))
+  (*((so_functions::t_AM_API_DisposeRequest)so_functions::so->AM_API_DisposeRequest))
     (pRequest);
 }
 
 void AM_API_DisposeResponses(AMResponses *responses){
-  (*((so_functions::t_AM_API_DisposeResponses)so.AM_API_DisposeResponses))
+  (*((so_functions::t_AM_API_DisposeResponses)so_functions::so->AM_API_DisposeResponses))
     (responses);
 }
 
 int AM_API_GetResponseScoresNum(AMResponse *response, int *n_scores){
-  return (*((so_functions::t_AM_API_GetResponseScoresNum)so.AM_API_GetResponseScoresNum))
+  return (*((so_functions::t_AM_API_GetResponseScoresNum)so_functions::so->AM_API_GetResponseScoresNum))
     (response, n_scores);
 }
 int AM_API_GetName(AlgoMarker * pAlgoMArker, char **name){
-  return (*((so_functions::t_AM_API_GetName)so.AM_API_GetName))
+  return (*((so_functions::t_AM_API_GetName)so_functions::so->AM_API_GetName))
     (pAlgoMArker, name);
 }
 int AM_API_GetScoreMessages(AMResponse *response, int score_index, int *n_msgs, int **msgs_codes, char ***msgs_args){
-  return (*((so_functions::t_AM_API_GetScoreMessages)so.AM_API_GetScoreMessages))
+  return (*((so_functions::t_AM_API_GetScoreMessages)so_functions::so->AM_API_GetScoreMessages))
     (response, score_index, n_msgs, msgs_codes, msgs_args);
 }
 int AM_API_GetResponseMessages(AMResponse *response, int *n_msgs, int **msgs_codes, char ***msgs_args){
-  return (*((so_functions::t_AM_API_GetResponseMessages)so.AM_API_GetResponseMessages))
+  return (*((so_functions::t_AM_API_GetResponseMessages)so_functions::so->AM_API_GetResponseMessages))
     (response, n_msgs, msgs_codes, msgs_args);
 }
 int AM_API_GetResponseScoreByType(AMResponses *responses,int res_index, char *_score_type, float *out_score){
-  return (*((so_functions::t_AM_API_GetResponseScoreByType)so.AM_API_GetResponseScoreByType))
+  return (*((so_functions::t_AM_API_GetResponseScoreByType)so_functions::so->AM_API_GetResponseScoreByType))
     (responses, res_index, _score_type, out_score);
 }
 int AM_API_GetResponseScoreByIndex(AMResponse *response, int score_index, float *score, char **_score_type){
-  return (*((so_functions::t_AM_API_GetResponseScoreByIndex)so.AM_API_GetResponseScoreByIndex))
+  return (*((so_functions::t_AM_API_GetResponseScoreByIndex)so_functions::so->AM_API_GetResponseScoreByIndex))
     (response,score_index,score,_score_type);
 }
 
 int AM_API_GetResponsePoint(AMResponse *response, int *pid, long long *timestamp){
-  return (*((so_functions::t_AM_API_GetResponsePoint)so.AM_API_GetResponsePoint))
+  return (*((so_functions::t_AM_API_GetResponsePoint)so_functions::so->AM_API_GetResponsePoint))
     (response,pid,timestamp);
 }
 
 int AM_API_GetSharedMessages(AMResponses *responses, int *n_msgs, int **msgs_codes, char ***msgs_args){
-  return (*((so_functions::t_AM_API_GetSharedMessages)so.AM_API_GetSharedMessages))
+  return (*((so_functions::t_AM_API_GetSharedMessages)so_functions::so->AM_API_GetSharedMessages))
     (responses,n_msgs,msgs_codes,msgs_args);
 }
 
 int AM_API_GetResponsesNum(AMResponses *responses){
-   return (*((so_functions::t_AM_API_GetResponsesNum)so.AM_API_GetResponsesNum))
+   return (*((so_functions::t_AM_API_GetResponsesNum)so_functions::so->AM_API_GetResponsesNum))
      (responses);
 }
 int AM_API_GetResponseIndex(AMResponses *responses, int _pid, long long _timestamp){
-   return (*((so_functions::t_AM_API_GetResponseIndex)so.AM_API_GetResponseIndex))
+   return (*((so_functions::t_AM_API_GetResponseIndex)so_functions::so->AM_API_GetResponseIndex))
      (responses,_pid,_timestamp);
 }
 int AM_API_GetResponseIndex(AMResponses *responses, char **requestId){
-   return (*((so_functions::t_AM_API_GetResponsesRequestId)so.AM_API_GetResponsesRequestId))
+   return (*((so_functions::t_AM_API_GetResponsesRequestId)so_functions::so->AM_API_GetResponsesRequestId))
      (responses, requestId);
 }
 
 int AM_API_GetResponseAtIndex(AMResponses *responses, int index, AMResponse **response){
-   return (*((so_functions::t_AM_API_GetResponseAtIndex)so.AM_API_GetResponseAtIndex))
+   return (*((so_functions::t_AM_API_GetResponseAtIndex)so_functions::so->AM_API_GetResponseAtIndex))
      (responses,index,response);
 }
 
 int AM_API_Create(int am_type, AlgoMarker **new_am){
-  return (*((so_functions::t_AM_API_Create)so.AM_API_Create))
+  return (*((so_functions::t_AM_API_Create)so_functions::so->AM_API_Create))
     (am_type, new_am);
 }
 
 int AM_API_Load(AlgoMarker * pAlgoMarker, const char *config_fname){
-  return (*((so_functions::t_AM_API_Load)so.AM_API_Load))
+  return (*((so_functions::t_AM_API_Load)so_functions::so->AM_API_Load))
     (pAlgoMarker, config_fname);
 }
 
 int AM_API_AddData(AlgoMarker * pAlgoMarker, int patient_id, const char *signalName, int TimeStamps_len, long long* TimeStamps, int Values_len, float* Values){
-  return (*((so_functions::t_AM_API_AddData)so.AM_API_AddData))
+  return (*((so_functions::t_AM_API_AddData)so_functions::so->AM_API_AddData))
     (pAlgoMarker, patient_id, signalName, TimeStamps_len, TimeStamps, Values_len, Values);
 }
 
-int AM_API_AddDataStr(AlgoMarker * pAlgoMarker, int patient_id, const char *signalName, int TimeStamps_len, long long* TimeStamps, int Values_len, float* Values) {
-	return (*((so_functions::t_AM_API_AddData)so.AM_API_AddDataStr))
+int AM_API_AddDataStr(AlgoMarker * pAlgoMarker, int patient_id, const char *signalName, int TimeStamps_len, long long* TimeStamps, int Values_len, char** Values) {
+	return (*((so_functions::t_AM_API_AddDataStr)so_functions::so->AM_API_AddDataStr))
 		(pAlgoMarker, patient_id, signalName, TimeStamps_len, TimeStamps, Values_len, Values);
 }
 
 
 int AM_API_CreateRequest(char *requestId, char **score_types, int n_score_types, int *patient_ids, long long *time_stamps, int n_points, AMRequest **new_req){
-  return (*((so_functions::t_AM_API_CreateRequest)so.AM_API_CreateRequest))
+  return (*((so_functions::t_AM_API_CreateRequest)so_functions::so->AM_API_CreateRequest))
     (requestId,score_types,n_score_types,patient_ids,time_stamps,n_points,new_req);
 }
 
 int AM_API_CreateResponses(AMResponses **new_responses){
-  return (*((so_functions::t_AM_API_CreateResponses)so.AM_API_CreateResponses))
+  return (*((so_functions::t_AM_API_CreateResponses)so_functions::so->AM_API_CreateResponses))
     (new_responses);
 }
 
 int AM_API_Calculate(AlgoMarker *pAlgoMarker, AMRequest *request, AMResponses *responses){
-  return (*((so_functions::t_AM_API_Calculate)so.AM_API_Calculate))
+  return (*((so_functions::t_AM_API_Calculate)so_functions::so->AM_API_Calculate))
     (pAlgoMarker, request, responses);
 }
