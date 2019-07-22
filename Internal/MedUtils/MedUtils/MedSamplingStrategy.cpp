a04d3ce14ba67c19a0b77b3e1d553d73a13aaa6b
Alon Lanyado <alon@earlysign.com>
2019-07-22 11:48:48 +0300

    MedSamplingFixedTime: add_time support for years

--- Internal/MedUtils/MedUtils/MedSamplingStrategy.cpp
+++ Internal/MedUtils/MedUtils/MedSamplingStrategy.cpp
@@ -350,21 +350,29 @@ int MedSamplingFixedTime::init(map<string, string>& map) {
 }
 
 int MedSamplingFixedTime::add_time(int time, int add) const {
+	//otherwise will lose month ad day in convertion
+	if (time_range_unit == MedTime::Date && time_jump_unit == MedTime::Months) {
+		int tm_month = (time % 10000) / 100 + add;
+		return 10000 * (int(time / 10000) + int((tm_month - 1) / 12)) + 100 * (1 + (tm_month % 12)) + (time % 100);
+	}
+	if (time_range_unit == MedTime::Date && time_jump_unit == MedTime::Years)
+		return time + add * 10000;
+
 	int conv = med_time_converter.convert_times(time_range_unit, time_jump_unit, time) + add;
 	return med_time_converter.convert_times(time_jump_unit, time_range_unit, conv);
 }
 
 bool validate_in_dates(const vector<pair<int, int>> &pid_dates, int pred_date, const TimeWindowMode &interaction) {
 	bool inside = false;
-	
+
 	for (size_t i = 0; i < pid_dates.size() && !inside; ++i)
-		inside = medial::sampling::in_time_window_simple(pred_date, 
+		inside = medial::sampling::in_time_window_simple(pred_date,
 			pid_dates[i].first, pid_dates[i].second, false, interaction);
-		//inside = pred_date >= pid_dates[i].first && pred_date <= pid_dates[i].second;
+	//inside = pred_date >= pid_dates[i].first && pred_date <= pid_dates[i].second;
 	return inside;
 }
 
-void filter_opts(unordered_map<int, vector<int>> &pid_options, 
+void filter_opts(unordered_map<int, vector<int>> &pid_options,
 	const unordered_map<int, vector<pair<int, int>>> &pid_time_ranges, const TimeWindowMode &interaction) {
 	unordered_map<int, vector<int>> pid_filtered;
 	for (auto it = pid_options.begin(); it != pid_options.end(); ++it)
