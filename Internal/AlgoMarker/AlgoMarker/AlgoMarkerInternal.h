277aa827dfa58d21fcba7854fbb66727a95987d0
Shlomi Loubaton <shlomi@medial-research.com>
2019-08-06 17:06:27 +0300

    Print error message on AM missing req signals error

--- Internal/AlgoMarker/AlgoMarker/AlgoMarkerInternal.h
+++ Internal/AlgoMarker/AlgoMarker/AlgoMarkerInternal.h
@@ -64,12 +64,15 @@ public:
 	// init model
 	int init_model_from_file(const char *_model_fname) { model.clear();	model.verbosity = 0; return (model.read_from_file(string(_model_fname))); }
 	int model_check_required_signals() { 
+		int ret = 0;
 		vector<string> req_sigs;
 		model.get_required_signal_names(req_sigs);
 		for (const auto& s : req_sigs)
-			if (0 == rep.sigs.Name2Sid.count(s))
-				return -1;
-		return 0;
+			if (0 == rep.sigs.Name2Sid.count(s)) {
+				ret = -1;
+				fprintf(stderr, "ERROR: AM model requires signal '%s' but signal does not exist in AM repository .signals file\n", s.c_str());
+			}
+		return ret;
 	}
 
 
