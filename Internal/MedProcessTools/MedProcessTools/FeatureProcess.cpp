afeec58c9e7eca31c83908a9fd9d25fe8e76bbd0
Ron <Ron@RON-SECURED>
2020-06-17 13:59:10 +0300

    added missing value indicator feature processor

--- Internal/MedProcessTools/MedProcessTools/FeatureProcess.cpp
+++ Internal/MedProcessTools/MedProcessTools/FeatureProcess.cpp
@@ -56,6 +56,8 @@ FeatureProcessorTypes feature_processor_name_to_type(const string& processor_nam
 		return FTR_PROCESS_RESAMPLE_WITH_MISSING;
 	else if (processor_name == "duplicate")
 		return FTR_PROCESS_DUPLICATE;
+	else if (processor_name == "missing_indicator")
+		return FTR_PROCESS_MISSING_INDICATOR;
 	else
 		MTHROW_AND_ERR("feature_processor_name_to_type got unknown processor_name [%s]\n", processor_name.c_str());
 }
@@ -144,6 +146,9 @@ FeatureProcessor * FeatureProcessor::make_processor(FeatureProcessorTypes proces
 		return new ResampleMissingProcessor;
 	else if (processor_type == FTR_PROCESS_DUPLICATE)
 		return new DuplicateProcessor;
+	else if (processor_type == FTR_PROCESS_MISSING_INDICATOR)
+		return new MissingIndicatorProcessor;
+
 	else
 		MTHROW_AND_ERR("make_processor got unknown processor type [%d]\n", processor_type);
 
@@ -1402,6 +1407,72 @@ int GetProbFeatProcessor::init(map<string, string>& mapper) {
 	return 0;
 }
 
+
+//=======================================================================================
+// MissingIndicatorProcessor
+//=======================================================================================
+
+int MissingIndicatorProcessor::init(map<string, string>& mapper) {
+	bool replace_value_exists = false;
+	for (auto entry : mapper) {
+		string field = entry.first;
+		//! [GetProbFeatProcessor::init]
+		if (field == "name") feature_name = entry.second;
+		else if (field == "missing_value") missing_value = stof(entry.second);
+		else if (field == "replace_value") {
+			replace_value = stof(entry.second);
+			replace_value_exists = true;
+		}
+		else if (field != "names" && field != "fp_type" && field != "tag")
+			MLOG("Unknonw parameter \'%s\' for FeatureMissingIndicator\n", field.c_str());
+	}
+	if (!replace_value_exists)
+		replace_value = missing_value;
+
+	return 0;
+}
+
+
+int MissingIndicatorProcessor::_apply(MedFeatures& features, unordered_set<int>& ids)
+{
+	// Resolve
+	resolved_feature_name = resolve_feature_name(features, feature_name);
+
+	new_feature_name = resolved_feature_name + "." + name;
+
+#pragma omp critical
+	{
+		features.data[new_feature_name].clear();
+		// Attributes
+		features.attributes[new_feature_name].normalized = false;
+		features.attributes[new_feature_name].imputed = true;
+	}
+
+	for (auto &val : features.data[resolved_feature_name])
+	{
+		if (val == missing_value) {
+			features.data[new_feature_name].push_back(1.);
+			if (val != replace_value)
+#pragma omp critical
+			val = replace_value;
+		}
+		else
+			features.data[new_feature_name].push_back(0.);
+	}
+	return 0;
+}
+
+void MissingIndicatorProcessor::update_req_features_vec(unordered_set<string>& out_req_features, unordered_set<string>& in_req_features)
+{
+	// If empty, keep as is
+	if (out_req_features.empty())
+		in_req_features.clear();
+	else {
+		in_req_features = out_req_features;
+		if (out_req_features.find(new_feature_name) != out_req_features.end())
+			in_req_features.insert(resolved_feature_name);
+	}
+}
 
 //=======================================================================================
 // Utilities
