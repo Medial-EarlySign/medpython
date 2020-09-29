8d26bd3be985437a32221383890bb76464f681c3
Alon Lanyado <alon@earlysign.com>
2020-09-29 14:37:46 +0300

    fix doxygen

--- Internal/InfraMed/InfraMed/InfraMed.h
+++ Internal/InfraMed/InfraMed/InfraMed.h
@@ -480,13 +480,13 @@ public:
 
 	int generate_fnames_for_prefix();
 
-	///<summary>
+	/// <summary>
 	///Option to read additional dictionary - should be called before!! read_all or init. Can be called multiple time for multiple dicts
-	///</summary>
+	/// </summary>
 	void load_additional_dict(const string &dict_path);
-	///<summary>
+	/// <summary>
 	///Clear additional dict list
-	///</summary>
+	/// </summary>
 	void clear_additional_dict();
 private:
 	int get_data_mode(const string &fname);
