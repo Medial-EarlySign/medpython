1573629fe564bebca03d9a7b0c6b92d2615bd57d
Alon Lanyado <alon@earlysign.com>
2020-12-09 21:55:08 +0200

    change_prior: keep split info

--- Internal/MedProcessTools/MedProcessTools/MedFeatures.cpp
+++ Internal/MedProcessTools/MedProcessTools/MedFeatures.cpp
@@ -1849,7 +1849,7 @@ double medial::process::match_to_prior(MedSamples &samples, float target_prior, 
 		medial::process::match_to_prior(fetched_labels, all_in_same, target_prior, sel_idx);
 		medial::process::commit_selection(pointers_to_smps, sel_idx);
 		//sort by pid:
-		sort(pointers_to_smps.begin(), pointers_to_smps.end(), [](MedSample *a, MedSample *b) {
+		sort(pointers_to_smps.begin(), pointers_to_smps.end(), [](const MedSample *a, const MedSample *b) {
 			if (a->id == b->id)
 				return b->time > a->time;
 			return b->id > a->id;
@@ -1859,12 +1859,14 @@ double medial::process::match_to_prior(MedSamples &samples, float target_prior, 
 		{
 			if (to_change.empty() || to_change.back().id != pointers_to_smps[i]->id) {
 				MedIdSamples smp(pointers_to_smps[i]->id);
+				smp.split = pointers_to_smps[i]->split;
 				to_change.push_back(smp);
 			}
 			to_change.back().samples.push_back(*pointers_to_smps[i]);
 		}
 		MLOG("Changing prior: was %2.3f%% and changed to %2.3f%%\n", 100 * pr, 100 * target_prior);
-		samples.idSamples.swap(to_change);
+		samples.idSamples = move(to_change);
+		samples.sort_by_id_date();
 		medial::print::print_samples_stats(samples);
 	}
 	return pr;
