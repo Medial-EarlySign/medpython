ced719eb84f52f1f5476e8fb57d8d47c4cc0a35b
Alon Lanyado <alon@earlysign.com>
2021-01-06 16:26:57 +0200

    update make.sh

--- Internal/MedPyExport/generate_binding/make.sh
+++ Internal/MedPyExport/generate_binding/make.sh
@@ -3,6 +3,7 @@
 DIST_NAME=${1-unknown}
 if [ $DIST_NAME == "unknown" ]; then
 	if [[ ${PYTHON_INCLUDE_DIR} == *"/python36/"* ]]; then DIST_NAME="medial-python36"
+	elif [[ ${PYTHON_INCLUDE_DIR} == *"/python38"* ]]; then DIST_NAME="medial-python38"
 	elif [[ ${PYTHON_INCLUDE_DIR} == "/opt/medial/python27"* ]]; then DIST_NAME="medial-python27"
 	elif [[ ${PYTHON_INCLUDE_DIR} == *"anaconda2"* ]]; then DIST_NAME="anaconda2"
 	elif [[ ${PYTHON_INCLUDE_DIR} == "/usr"* ]]; then DIST_NAME="rh-python27"
