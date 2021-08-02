cb75c16a6a8d73fee4f579137618e0fe5d31080b
Alon Lanyado <alon@earlysign.com>
2021-08-02 10:56:37 +0300

    MPDictionary: added missing argument of section_id to name,id functions

--- Internal/MedPyExport/MedPyExport/MPDictionary.cpp
+++ Internal/MedPyExport/MedPyExport/MPDictionary.cpp
@@ -17,9 +17,9 @@ MPDictionary::MPDictionary(MPPidRepository* rep): o(rep->o) { }
 
 int MPDictionary::section_id(string name) { return o->dict.section_id(name); }
 
-int MPDictionary::id(string& signame) { return o->dict.id(signame); }
+int MPDictionary::id(int section_id, string& signame) { return o->dict.id(section_id, signame); }
 
-string MPDictionary::name(int id) { return o->dict.name(id); };
+string MPDictionary::name(int section_id, int id) { return o->dict.name(section_id, id); };
 
 void MPDictionary::prep_sets_lookup_table(int section_id, const std::vector<string>& set_names, MEDPY_NP_OUTPUT(char** lut_array, unsigned long long* lut_size)) {
 	std::vector<char> lut;
--- Internal/MedPyExport/MedPyExport/MPDictionary.h
+++ Internal/MedPyExport/MedPyExport/MPDictionary.h
@@ -13,8 +13,8 @@ public:
 	MPDictionary(MPPidRepository* rep);
 
 	int section_id(string name);
-	int id(string& signame);
-	string name(int id);
+	int id(int section_id, string& signame);
+	string name(int section_id, int id);
 	void prep_sets_lookup_table(int section_id, const std::vector<string>& set_names, MEDPY_NP_OUTPUT(char** lut_array, unsigned long long* lut_size));
 
 	MPIntVecIntMapAdaptor get_members_to_all_sets(int section_id, MEDPY_NP_INPUT(int* members_array, unsigned long long members_size));
