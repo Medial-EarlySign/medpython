2d5dd2dd7880b0f01412ed2fca896cfaacd4d259
Alon Lanyado <alon@earlysign.com>
2021-09-26 19:29:32 +0300

    more error info

--- Internal/MedProcessTools/MedProcessTools/BinnedLmEstimates.cpp
+++ Internal/MedProcessTools/MedProcessTools/BinnedLmEstimates.cpp
@@ -181,7 +181,7 @@ int BinnedLmEstimates::_learn(MedPidRepository& rep, const MedSamples& samples, 
 
 	// Sanity check
 	if (signalId == -1 || genderId == -1 || bdateId == -1) {
-		MERR("BinnedLmEstimates::_learn - Uninitialized signalId\n");
+		MERR("BinnedLmEstimates::_learn - Uninitialized signalId (%s)\n", signalName.c_str());
 		return -1;
 	}
 
@@ -484,7 +484,7 @@ int BinnedLmEstimates::_generate(PidDynamicRec& rec, MedFeatures& features, int 
 
 	// Sanity check
 	if (signalId == -1 || genderId == -1 || bdateId == -1) {
-		MERR("BinnedLmEstimates::_generate - Uninitialized signalId\n");
+		MERR("BinnedLmEstimates::_generate - Uninitialized signalId(%)\n", signalName.c_str());
 		return -1;
 	}
 
--- Internal/MedProcessTools/MedProcessTools/RepCategoryDescenders.cpp
+++ Internal/MedProcessTools/MedProcessTools/RepCategoryDescenders.cpp
@@ -50,7 +50,7 @@ int RepCategoryDescenders::_apply(PidDynamicRec& rec, vector<int>& time_points,
 	vector<vector<float>>& attributes_mat) {
 	// Sanity check
 	if (signalId == -1) {
-		MERR("RepCategoryDescenders::_apply - Uninitialized signalId\n");
+		MERR("RepCategoryDescenders::_apply - Uninitialized signalId(%s)\n", signalName.c_str());
 		return -1;
 	}
 
