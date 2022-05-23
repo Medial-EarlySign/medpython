e1462d0738eb683dff8cfe18793938340794ff47
Ilya-internal <Ilya-internal@ILYA-SECURE>
2022-05-23 09:40:36 +0300

    Interface change:switch from referrring to KFRE version vs. model_id to using n_variables. Model 6 => 8-variable KFRE equation Model 3 => 4-variable KFRE equation Model 2 => 3-variable KFRE equation

--- Internal/MedProcessTools/MedProcessTools/RepCalculators.cpp
+++ Internal/MedProcessTools/MedProcessTools/RepCalculators.cpp
@@ -85,10 +85,17 @@ bool RatioCalculator::do_calc(const vector<float> &vals, float &res) const {
 }
 //.......................................KFRE Calculator................................
 int KfreCalculator::init(map<string, string>& mapper) {
+	
 	for (auto it = mapper.begin(); it != mapper.end(); ++it)
 	{
-		if (it->first == "model_id")
-			model_id = stoi(it->second);
+		if (it->first == "n_variables")
+			n_variables = stoi(it->second);
+		if (it->first == "prediction_years")
+			prediction_years = stoi(it->second);
+		if (it->first == "publication_year")
+			publication_year = stoi(it->second);
+		if (it->first == "region")
+			region = it->second;
 		else if (it->first == "discard_range_check")
 			discard_range_check = stoi(it->second) > 0;
 		else if (it->first == "keep_only_in_range") {
@@ -98,6 +105,7 @@ int KfreCalculator::init(map<string, string>& mapper) {
 		else 
 			MTHROW_AND_ERR("Error in KfreCalculator::init - Unsupported argument \"%s\"\n",it->first.c_str());
 	}
+
 	return 0;
 }
 
@@ -108,27 +116,16 @@ void KfreCalculator::validate_arguments(const vector<string> &input_signals, con
 	and that names of the input parameters are correct.
 	*/
 
+	// Validate the number of output variables
 	if (!(output_signals.size() == 1))
 		MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Requires exactly 1 output signal\n");
 
-	switch (model_id)
-	{
-	case 2:
-		//Signals order: "BDATE", "GENDER", "eGFR"
-		if (!(input_signals.size() == 3))
-			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Requires 3 input signals -  got \"%d\"\n", (int)input_signals.size());
-		break;
-	case 3:
-		if (!(input_signals.size() == 4))
-			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Requires 4 input signals -  got \"%d\"\n", (int)input_signals.size());
-		break;
-	case 6:
-		if (!(input_signals.size() == 8))
-			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Requires 8 input signals -  got \"%d\"\n", (int)input_signals.size());
-		break;
-	}
+	// Validate the number of input variables
+	if (!(input_signals.size() == n_variables))
+		MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Requires %d input signals -  got \"%d\"\n", n_variables, (int)input_signals.size());
 
-	if (model_id >= 2)
+	// Verify that variable names are as expected
+	if (n_variables >= 3)
 	{
 		// NOTE: "break" statements are missing on purpose
 		if (input_signals[0] != "BDATE")
@@ -138,13 +135,13 @@ void KfreCalculator::validate_arguments(const vector<string> &input_signals, con
 		if (input_signals[2] != "eGFR_CKD_EPI")
 			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Third signal should be eGFR_CKD_EPI- got \"%s\"\n", input_signals[2].c_str());
 	}
-	if (model_id >= 3)
+	if (n_variables >= 4)
 	{
 		if (input_signals[3] != "UrineAlbumin_over_Creatinine")
 			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Fourth signal should be UrineAlbumin_over_Creatinine- got \"%s\"\n", input_signals[3].c_str());
 	}
 
-	if (model_id >= 6)
+	if (n_variables >= 8)
 	{
 		if (input_signals[4] != "Ca")
 			MTHROW_AND_ERR("Error KfreCalculator::validate_arguments - Fifth signal should be Ca - got \"%s\"\n", input_signals[4].c_str());
@@ -179,30 +176,14 @@ bool KfreCalculator::do_calc(const vector<float> &vals, float &res) const {
 	res = missing_value;
 
 	// Verify that the model id is valid
-	if (model_id != 2 && model_id != 3 && model_id != 6)
-		MTHROW_AND_ERR("Error KfreCalculator::do_calc - model_id should be either 2,3 or 6\n");
+	if (n_variables != 3 && n_variables != 4 && n_variables != 8)
+		MTHROW_AND_ERR("Error KfreCalculator::do_calc - n_variables should be either 3,4 or 8\n");
 
 	// Verify that the number of arguments is correct
-	switch (model_id)
-	{
-	case 2:
-		if (vals.size() != 4)
-			MTHROW_AND_ERR("Error KfreCalculator::do_calc -wrong number of arguments. expected 4, got %zu\n",
-				vals.size());
-		break;
-	case 3:
-		if (vals.size() != 5)
-			MTHROW_AND_ERR("Error KfreCalculator::do_calc -wrong number of arguments. expected 4, got %zu\n",
-				vals.size());
-		break;
-	case 6:
-		if (vals.size() != 9)
-			MTHROW_AND_ERR("Error KfreCalculator::do_calc -wrong number of arguments. expected 8, got %zu\n",
-				vals.size());
-		break;
-	default:
-		MTHROW_AND_ERR("Error KfreCalculator::do_calc -wrong model_id. expected 2,3, or 6, got %d\n", model_id)
-	}
+	// (one more than n_variables, due to the "time"
+	if (vals.size() != n_variables+1)
+		MTHROW_AND_ERR("Error KfreCalculator::do_calc -wrong number of arguments. expected %d, got %zu\n",
+			n_variables+1,vals.size());
 
 	// Prepare and validate input arguments
 
@@ -215,7 +196,7 @@ bool KfreCalculator::do_calc(const vector<float> &vals, float &res) const {
 	bdate = (int)vals[0]; //BDATE
 	year = int(bdate / 10000);
 
-	if (model_id >= 2)
+	if (n_variables >= 3)
 	{
 		// age
 		time = (int)vals.back(); //TIME
@@ -235,7 +216,7 @@ bool KfreCalculator::do_calc(const vector<float> &vals, float &res) const {
 			range_check_failed = true;
 	}
 
-	if (model_id >= 3)
+	if (n_variables >= 4)
 	{
 		UACR = vals[3];
 
@@ -246,7 +227,7 @@ bool KfreCalculator::do_calc(const vector<float> &vals, float &res) const {
 			range_check_failed = true;
 	}
 
-	if (model_id >= 6)
+	if (n_variables >= 8)
 	{
 		Calcium		= vals[4];
 		if (Calcium < 7.5 || Calcium > 10.5)
@@ -275,17 +256,15 @@ bool KfreCalculator::do_calc(const vector<float> &vals, float &res) const {
 			return !keep_only_in_range;
 		}
 			
-
-
-	switch (model_id)
+	switch (n_variables)
 	{
-	case 2:
+	case 3:
 		res = get_KFRE_Model_2(age, gender, eGFR);
 		break;
-	case 3:
+	case 4:
 		res = get_KFRE_Model_3(age, gender, eGFR, UACR);
 		break;
-	case 6:
+	case 8:
 		bool valid = get_KFRE_Model_6(
 			res, 
 			age,
