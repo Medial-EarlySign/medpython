7d2cd9886d538d322b99d8557c1b51060265cda8
Alon Lanyado <alon@earlysign.com>
2023-04-09 12:22:29 +0300

    bugfix

--- Internal/AlgoMarker/AlgoMarker/MedialInfra_AM.cpp
+++ Internal/AlgoMarker/AlgoMarker/MedialInfra_AM.cpp
@@ -300,7 +300,7 @@ void process_explainability(nlohmann::ordered_json &jattr, const Explainer_param
 				if (level_bin > 1)
 					level_bin = 1;
 				level_bin *= 100;
-				level_bin = round(level_bin / ex_params.num_groups);
+				level_bin = ex_params.num_groups * round(level_bin / ex_params.num_groups);
 
 				e["contributor_level_group"] = (int)level_bin;
 			}
