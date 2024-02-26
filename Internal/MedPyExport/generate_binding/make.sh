2e7770a06585dbe95c7fcecbed1b8845b344c328
Alon Lanyado <alon@earlysign.com>
2024-02-27 00:17:22 +0200

    Add build of static lib to python compile

--- Internal/MedPyExport/generate_binding/make.sh
+++ Internal/MedPyExport/generate_binding/make.sh
@@ -25,7 +25,75 @@ if [ $SKIP_PROMPT -lt 1 ]; then
 	read -p "Press [Enter] to approve"
 fi
 
+BUILD_BOOST=true
+BUILD_XGBOOST=true
+BUILD_LIGHTGBM=true
+BUILD_ALGOMARKER=true
 
+OVERRIDE_ALL=${1-0}
+
+BOOST_FPIC_DIR=/nas1/Work/Libs/Boost/boost_1_67_0-fPIC.ubuntu
+DIR=$MR_ROOT/Libs/Internal/AlgoMarker/LinuxSharedLib
+BLDDIR="${DIR}/Build.ubuntu"
+STATIC_LIBS_TARGET="${BLDDIR}/lib"
+
+mkdir -p ${BLDDIR}
+mkdir -p ${STATIC_LIBS_TARGET}
+
+if [[ "`which sudo 2>>/dev/null`" ]]; 
+then 
+export SUDO="`which sudo`"
+else
+export SUDO=''
+fi
+
+echo "SUDO=${SUDO}"
+echo "BOOST_FPIC_DIR=${BOOST_FPIC_DIR}"
+echo "DIR=${DIR}"
+echo "BLDDIR=${BLDDIR}"
+echo "STATIC_LIBS_TARGET=${STATIC_LIBS_TARGET}"
+
+mkdir -p "${STATIC_LIBS_TARGET}"
+mkdir -p ${BOOST_FPIC_DIR}/installation
+source /nas1/Work/python-env/python310/bin/activate
+
+if [ "$BUILD_BOOST" = true ] ; then
+pushd ${BOOST_FPIC_DIR}
+if [ ! -f ${STATIC_LIBS_TARGET}/libboost_system.a ] || [ $OVERRIDE_ALL -gt 0 ]; then
+	./b2 --clean
+	./bootstrap.sh --without-icu --with-libraries=filesystem,system,regex,program_options --prefix=${BOOST_FPIC_DIR}/installation
+	./b2 link=static variant=release linkflags=-static-libstdc++ -j20 cxxflags="-fPIC" toolset=gcc --stagedir="${BLDDIR}"
+else
+	echo "BOOST Already exists"
+fi
+popd
+fi
+
+if [ "$BUILD_XGBOOST" = true ] ; then
+pushd $MR_ROOT/Libs/External/xgboost
+if [ $OVERRIDE_ALL -gt 0 ]; then
+make clean
+cp $MR_ROOT/Libs/External/xgboost/CMakeLists.txt.orig $MR_ROOT/Libs/External/xgboost/CMakeLists.txt
+cmake .
+
+#The clean doesn't delete .a files
+find . -name '*.a' -exec rm {} \;
+fi
+make -j20
+cp ./lib/libxgboost.a ${STATIC_LIBS_TARGET}
+cp ./rabit/lib/librabit.a ${STATIC_LIBS_TARGET}
+cp ./dmlc-core/libdmlc.a ${STATIC_LIBS_TARGET}
+popd
+fi
+
+if [ "$BUILD_LIGHTGBM" = true ] ; then
+mkdir -p ${BLDDIR}/LightGBM/
+cp ${DIR}/Resources/LightGBM.ubuntu/LightGBM_CMakeLists_file ${BLDDIR}/LightGBM/CMakeLists.txt
+pushd ${BLDDIR}/LightGBM/
+cmake .
+make -j20
+popd 
+fi
 
 cp SWIG.CMakeLists.txt CMakeLists.txt
 cp MedPython/SWIG.CMakeLists.txt MedPython/CMakeLists.txt
