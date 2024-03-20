78f345a2c40114622014b1833188d01d8c804352
Alon Lanyado <alon@earlysign.com>
2024-03-20 22:26:47 +0200

    Serialization: support complition on both old c++11 and c++14 and beyond

--- Internal/SerializableObject/SerializableObject/SerializableObject_imp.h
+++ Internal/SerializableObject/SerializableObject/SerializableObject_imp.h
@@ -4,9 +4,11 @@
 #include <type_traits>
 #include <typeinfo>
 
-#if __GNUC__ 
+#if __GNUC__
+#if __cplusplus <= 201402L
 template< bool B, class T = void >
 using enable_if_t = typename enable_if<B, T>::type;
+#endif
 #endif // !_
 
 namespace MedSerialize {
