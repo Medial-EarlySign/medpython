fae260993a8c6602f1ad217ed7fad0576ee3bf89
Alon Lanyado <alonyado@gmail.com>
2025-08-14 20:50:24 +0300

    Removed unused code

--- External/vowpal_wabbit/vowpalwabbit/cs_active.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-LEARNER::base_learner* cs_active_setup(vw& all);
--- document_code_user.sh
+++ document_code_user.sh
@@ -2,5 +2,6 @@
 #sudo doxygen Doxyfile 2>&1 | egrep -v "External|QGDict|Please use a more specific name by including|Eigen::|VowpalWabbit|dmlc::|xgboost::|rabit::" > documentation_creation.log
 
 cd ${0%/*}
-sudo mkdir -p /home/$USER/html/Libs
-( cat Doxyfile ; echo "OUTPUT_DIRECTORY=/home/$USER/html/Libs" ) | sudo doxygen - 2>&1 | egrep -v "External|QGDict|Please use a more specific name by including|Eigen::|VowpalWabbit|dmlc::|xgboost::|rabit::" > documentation_creation_user.log
+OUTPUT_DIR=/home/$USER/html/Libs
+mkdir -p ${OUTPUT_DIR}
+( cat Doxyfile ; echo "OUTPUT_DIRECTORY=${OUTPUT_DIR}" ) | doxygen - 2>&1 | egrep -v "External|QGDict|Please use a more specific name by including|Eigen::|VowpalWabbit|dmlc::|xgboost::|rabit::" > documentation_creation_user.log
